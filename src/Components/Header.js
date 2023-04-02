a1f08c7d64e986fbebf1aa892f383642c7166edf
sharafath ali <sharafathaliofficial@gmail.com>
2023-04-02 21:51:32 +0530

    first

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      Header
+      <h1>gdvsbh</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      Main
+      <h1>giohs</h1>
+    </div>
+  );
+}
+
+export default Main;
