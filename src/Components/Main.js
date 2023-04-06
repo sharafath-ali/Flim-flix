88ace54a27df5c13ccbe8a07c290f2fba01b0c5b
sharafath ali <sharafathaliofficial@gmail.com>
2023-04-07 00:36:50 +0530

    card & maapped

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,27 +1,15 @@
 import React from "react";
-
+import { popularMovies } from "../data/Movielist";
+import Moviecard from "./Moviecard";
 function Main() {
   return (
     <main>
       <div className="movieSlider"></div>
       <div className="wrapper">
         <h2 className="sectionTitle">Popular Movies </h2>
-        <div className="gallery">
-          <div className="movieCard">
-            <img
-              className="moviePoster"
-              src="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-              alt=""
-            />
-            <div className="movieCardFooter">
-              <div className="movieCardDetails">
-                <h2 className="movieCardTitle">Avatar: The Way of Water</h2>
-                <p className="movieCardRating">7.7</p>
-              </div>
-              <div className="favButton">#</div>
-            </div>
-          </div>
-        </div>
+        {popularMovies.map((e) => {
+          return <Moviecard e={e} />;
+        })}
       </div>
     </main>
   );
---
+++ src/Components/Moviecard.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Moviecard(p) {
+  const { e } = p;
+  return (
+    <div className="gallery">
+      <div className="movieCard">
+        <img
+          className="moviePoster"
+          src="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+          alt=""
+        />
+        <div className="movieCardFooter">
+          <div className="movieCardDetails">
+            <h2 className="movieCardTitle">{e.title}</h2>
+            <p className="movieCardRating">{e.rating}</p>
+          </div>
+          <div className="favButton">#</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Moviecard;
